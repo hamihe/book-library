59c0bf2cdbdd09ad006f80bc680a459f8e82b7b8
hamihe <hh75662@hotmail.com>
2025-03-26 09:54:51 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BooksProvider } from "./BooksContext";
+import Books from "./components/Books";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import BookDetails from "./components/BookDetails";
+import NotFound from "./components/NotFound";
+import SecretBooks from "./components/SecretBooks";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BooksProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Books></Books>}></Route>
+          <Route path="/books" element={<Navigate to="/"></Navigate>}></Route>
+          <Route
+            path="/books/:bookId"
+            element={<BookDetails></BookDetails>}
+          ></Route>
+          <Route path="*" element={<NotFound></NotFound>}></Route>
+          <Route
+            path="/secret"
+            element={<PrivateRoute Component={<SecretBooks />}></PrivateRoute>}
+          ></Route>
+        </Routes>
+      </Router>
+    </BooksProvider>
   );
 }
 
